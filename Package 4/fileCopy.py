c67a1b3479950b23ea040ee346f9ea5adb1f4a30
Daniel <102466020+danielCodingGuy@users.noreply.github.com>
2023-12-29 13:41:25 +0100

    Add fileCopy.py

---
+++ Package 4/fileCopy.py
@@ -0,0 +1,7 @@
+#A simple program in python that uses copy methods.
+#author: danielCodingGuy
+
+import shutil
+shutil.copyfile("test.txt","copied.txt") #We need two files, the first one has the content and the second one is the one that the content of the first will be copied into.
+
+#Can also use the directories to select the files.
