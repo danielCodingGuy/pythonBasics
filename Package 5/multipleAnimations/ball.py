b4fe3f55c42ff01dca8eb02866eb805116707bf7
Daniel <102466020+danielCodingGuy@users.noreply.github.com>
2024-03-10 15:43:16 +0100

    Update ball.py

--- Package 5/multipleAnimations/ball.py
+++ Package 5/multipleAnimations/ball.py
@@ -1,6 +1,9 @@
+#Class that will create ball that multipleAnimations.py gonna move around.
+#author: danielCodingGuy
+
 class Ball:
 
-    def __init__(self, canvas, x, y, diameter, xVelocity, yVelocity, color):
+    def __init__(self, canvas, x, y, diameter, xVelocity, yVelocity, color): #This contains the coords of our object and some of its properties.
         self.canvas = canvas
         self.image = canvas.create_oval(x,y,diameter,diameter,fill = color)
         self.xVelocity = xVelocity
@@ -9,7 +12,7 @@ class Ball:
     def move(self):
         coordinates = self.canvas.coords(self.image)
         
-        if (coordinates[2]>=(self.canvas.winfo_width()) or coordinates[0]<0):
+        if (coordinates[2]>=(self.canvas.winfo_width()) or coordinates[0]<0): #Theese ifs make our ball bounce when coming in contact with borders of our window.
             self.xVelocity = -self.xVelocity
         if (coordinates[3]>=(self.canvas.winfo_height()) or coordinates[1]<0):
             self.yVelocity = -self.yVelocity
